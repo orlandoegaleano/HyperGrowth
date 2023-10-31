bd208897b74d9ba2c2921acbc06d71ed3f0a53d6
orlandoegaleano <125323979+orlandoegaleano@users.noreply.github.com>
2023-10-30 20:34:35 -0500

    Refactoring
    Refactored ExercisePickerPair out of CustomDay component so that the functionality can be used elsewhere

--- src/components/CustomDay.js
+++ src/components/CustomDay.js
@@ -1,122 +1,75 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Button, TouchableOpacity, Linking } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
-import { exercises, muscleGroups } from './exercises';
-import YouTubeButton from './YouTubeButton';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import ExercisePickerPair from './ExercisePickerPair';
 
+// Defining the CustomDay component.
 const CustomDay = (props) => {
-    // Create a state variable to store selected muscle groups and their exercises
-    // Initial state is an array with an empty object to render a Picker pair on initial start
+    // State for managing selected muscle groups. Initially set as an array with an empty object.
     const [selectedMuscleGroups, setSelectedMuscleGroups] = useState([{}]);
-    
-    // Function that adds a new exercise pair to the selectedMuscleGroups state
+
+    // Function to add a new exercise pair. It appends an empty object to the selectedMuscleGroups array.
     const addExercisePair = () => {
-        setSelectedMuscleGroups([...selectedMuscleGroups, {}]);  // Append an empty object to the current state
+        setSelectedMuscleGroups([...selectedMuscleGroups, {}]);
     };
 
-    // Function that removes an exercise pair based on its index
+    // Function to remove an exercise pair based on its index.
     const removeExercisePair = (indexToRemove) => {
+        // Filtering selectedMuscleGroups array to exclude the item with the given index.
         const filteredMuscleGroups = selectedMuscleGroups.filter((_, index) => index !== indexToRemove);
+        // Updating the state with the filtered array.
         setSelectedMuscleGroups(filteredMuscleGroups);
     };
 
+    // Handling updates to the selectedMuscleGroups array.
+    const handleUpdate = (index, key, value) => {
+        // Creating a copy of the current selectedMuscleGroups array.
+        const updatedMuscleGroups = [...selectedMuscleGroups];
+        // Updating the specified index with the given key-value pair.
+        updatedMuscleGroups[index][key] = value;
+        // Setting the updated array to the state.
+        setSelectedMuscleGroups(updatedMuscleGroups);
+    };
+
     return (
-        // Container View
+        
         <View style={styles.container}>
-            {/* Displaying the day number */}
-            <Text style={{alignSelf: 'center'}}>Day {props.id}</Text>
-            
-            {/* Map over the selectedMuscleGroups array to render pickers for each muscle group. 
-            The map function iterates over each item in the array, allowing for dynamic rendering based on the state.*/}
-            {selectedMuscleGroups.map((_, index) => (
-                // Render a View for each muscle group with a unique key based on its index
-                <View key={index} style={styles.picker}>
-                    {/* Picker is a dropdown list to select from a range of muscle groups.*/}
-                    <Picker
-                        // `selectedValue` is the value that is currently selected in the picker.
-                        // For this picker, it refers to the muscle from the current item being mapped over in `selectedMuscleGroups`.
-                        selectedValue={selectedMuscleGroups[index].muscle}
-
-                        // `onValueChange` is a callback that gets triggered when a user selects a different value in the picker.
-                        onValueChange={(itemValue) => {
-                            // Create a shallow copy of the `selectedMuscleGroups` state array.
-                            const updatedMuscleGroups = [...selectedMuscleGroups];
-
-                            // Update the muscle value for the current item to the newly selected value.
-                            updatedMuscleGroups[index].muscle = itemValue;
-
-                            // Reset the exercise value for the current item since the muscle group has changed.
-                            updatedMuscleGroups[index].exercise = undefined;
-
-                            // Update the `selectedMuscleGroups` state with the modified array.
-                            setSelectedMuscleGroups(updatedMuscleGroups);
-                        }}
-                    >
-
-                        {/* Picker.Item represents an individual selectable item in the Picker. 
-                        In this context, it's used to prompt the user to select a muscle group first.*/}
-                        <Picker.Item label="Please select a muscle group first" value={null}/>
-                        
-                        {/* Map over muscleGroups to render each as an individual selectable item.
-                        This allows for dynamic rendering based on available muscle groups.*/}
-                        {muscleGroups.map(group => (
-                            <Picker.Item key={group} label={group} value={group}/>
-                        ))}
-                    </Picker>
-                    <View style={{flexDirection: 'row', alignItems: 'center', marginRight: 5}}>
-                        {/* Another Picker component, this time for selecting an exercise associated with the chosen muscle group.
-                        It's dynamically populated based on the previously selected muscle group.*/}
-                        <Picker
-                            style={{flex: 9}}
-                            selectedValue={selectedMuscleGroups[index].exercise}
-                            onValueChange={(itemValue) => {
-                                const updatedMuscleGroups = [...selectedMuscleGroups];
-                                updatedMuscleGroups[index].exercise = itemValue;
-                                setSelectedMuscleGroups(updatedMuscleGroups);
-                            }}
-                        >
-                            <Picker.Item label="Please select an exercise" value={null}/>
-                            
-                            {/* Check if a muscle group has been selected. If yes, dynamically render exercises 
-                            associated with the chosen muscle group.*/}
-                            {selectedMuscleGroups[index].muscle && exercises[selectedMuscleGroups[index].muscle].map(exercise => (
-                                <Picker.Item key={exercise.name} label={exercise.name} value={exercise.name}/>
-                            ))}
-                        </Picker>
-                        <YouTubeButton 
-                            muscle={selectedMuscleGroups[index].muscle}
-                            exercise={selectedMuscleGroups[index].exercise}
-                            exercises={exercises}
-                        />
-
-                    </View>
-                    
-                    {/* Button to remove the current pair of Pickers */}
-                    <Button 
-                    title="Remove This Exercise" 
-                    onPress={() => removeExercisePair(index)}
-                    disabled={selectedMuscleGroups.length === 1}
+            {/* Displaying the day ID.*/}
+            <View style={{ backgroundColor: 'rgba(0,0,0,0.5)' }}>   
+                <Text style={{ alignSelf: 'center', fontSize: 30, fontWeight: 'bold', }}>Day {props.id}</Text>
+            </View> 
+            {/* Mapping through the selectedMuscleGroups array to display each exercise pair.*/}
+            {selectedMuscleGroups.map((muscleGroup, index) => (
+                // Using a View for each exercise pair.
+                <View key={index} style={{marginBottom: 10}}>
+                    {/* Including the ExercisePickerPair component and passing necessary props.*/}
+                    <ExercisePickerPair 
+                        index={index} 
+                        selectedMuscleGroup={muscleGroup} 
+                        onUpdate={handleUpdate} 
+                    />
+                    {/* Button to remove the current exercise pair.*/}
+                    <Button
+                        title="Remove This Exercise"
+                        onPress={() => removeExercisePair(index)}
+                        // Disabling the button if there's only one exercise pair.
+                        disabled={selectedMuscleGroups.length === 1}
                     />
                 </View>
             ))}
-            {/* Button component to add a new exercise pair */}
-            <Button 
-            title="Add Exercise" 
-            onPress={addExercisePair}/>
+            {/* Button to add a new exercise pair.*/}
+            <Button title="Add an exercise to this Day" onPress={addExercisePair} />
         </View>
     );
 };
 
-const styles = StyleSheet.create({
-    container: {
-        borderWidth: 1,
-        margin: 5,        
+const styles = StyleSheet.create({   
+    container: {        
+        margin: 5,       
     },
     picker: {
         borderWidth: 1,
-        borderColor: 'red',
+        borderColor: 'black',
     },
-
 });
 
 export default CustomDay;
---
+++ src/components/ExercisePickerPair.js
@@ -0,0 +1,66 @@
+import React from 'react'; 
+import { View, Button } from 'react-native'; 
+import { Picker } from '@react-native-picker/picker'; 
+import { exercises, muscleGroups } from './exercises';
+import YouTubeButton from './YouTubeButton'; 
+
+// Defining the ExercisePickerPair component.
+const ExercisePickerPair = ({ index, selectedMuscleGroup, onUpdate }) => {
+    return (
+        // A View container to wrap everything.
+        <View key={index} style={{borderWidth: 1, borderColor: 'red',}}>
+            
+            {/* The first Picker component to select muscle groups.*/}
+            <Picker
+                selectedValue={selectedMuscleGroup.muscle} // This sets the default value for the Picker to the currently selected muscle.
+                onValueChange={(itemValue) => { // A function that's called when a user selects a different muscle group.
+                    onUpdate(index, 'muscle', itemValue); // Updating the muscle for the current Picker.
+                    onUpdate(index, 'exercise', undefined); // Resetting the exercise when the muscle changes.
+                }}
+            >
+                {/* Default Picker item prompting user to make a selection.*/}
+                <Picker.Item label="Please select a muscle group first" value={null} />
+                
+                {/* Mapping through the muscleGroups array to dynamically generate Picker items for each muscle group.*/}
+                {muscleGroups.map(group => (
+                    // Each Picker.Item requires a unique 'key' for React's internal use.
+                    // 'label' is what's displayed to the user and 'value' is the actual value that gets selected.
+                    // In this case, 'group' is used for all three for simplicity since each muscle group has a unique name.
+                    <Picker.Item key={group} label={group} value={group} />
+                ))}
+            </Picker>
+            
+            {/* A View container to wrap the second Picker and the YouTubeButton.*/}
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginRight: 5 }}>
+                
+                {/* The second Picker component to select exercises based on the selected muscle group.*/}
+                <Picker
+                    style={{ flex: 9 }} // Giving it a flex value so it occupies 90% of the space in its container.
+                    selectedValue={selectedMuscleGroup.exercise} // This sets the default value for the Picker to the currently selected exercise.
+                    onValueChange={(itemValue) => { // A function that's called when a user selects a different exercise.
+                        onUpdate(index, 'exercise', itemValue); // Updating the exercise for the current Picker.
+                    }}
+                >
+                    {/* Default Picker item prompting user to make a selection.*/}
+                    <Picker.Item label="Please select an exercise" value={null} />
+                    
+                    {/* Checking if a muscle group is selected and then mapping through the exercises for that muscle group.*/}
+                    {selectedMuscleGroup.muscle && exercises[selectedMuscleGroup.muscle].map(exercise => (
+                        // For each exercise, a Picker.Item is created.
+                        <Picker.Item key={exercise.name} label={exercise.name} value={exercise.name} />
+                    ))}
+                </Picker>
+                
+                {/* YouTubeButton component to link to approprite video */}
+                <YouTubeButton
+                    muscle={selectedMuscleGroup.muscle}
+                    exercise={selectedMuscleGroup.exercise}
+                    exercises={exercises}
+                />
+            </View>
+        </View>
+    );
+};
+
+
+export default ExercisePickerPair;
