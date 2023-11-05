b78ab3d81c8debc0b361aaf2da79b99f9864d019
emejia3 <emejia3@uno.edu>
2023-11-05 14:04:01 -0600

    Able to add questions and answers with collapse feature for the answers

--- src/screens/HelpScreen.js
+++ src/screens/HelpScreen.js
@@ -1,20 +1,55 @@
-import React from 'react';
-import { Text, StyleSheet, View, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import NavBar from '../components/NavBar';
 
 const HelpScreen = () => {
-  return(
+  const initialData = [
+    { question: 'Question 1', answer: 'Answer 1', isCollapsed: true },
+    { question: 'Question 2', answer: 'Answer 2', isCollapsed: true },
+    // Add more questions, answers, and isCollapsed properties as needed
+    //This is just for testing until we get everything else is done.
+  ];
+
+  const [questions, setQuestions] = useState(initialData);
+
+  const toggleCollapse = (index) => {
+    // Create a copy of the questions array
+    const updatedQuestions = [...questions];
+    updatedQuestions[index].isCollapsed = !updatedQuestions[index].isCollapsed;
+    setQuestions(updatedQuestions);
+  };
+
+  return (
     <View>
-      <NavBar/>
-      <Text style={styles.text}>Hello HelpScreen</Text>
+      <NavBar />
+      {/* Map over the questions array and render each question */}
+      {questions.map((item, index) => (
+        <View key={index}>
+          <TouchableOpacity onPress={() => toggleCollapse(index)}>
+            <Text style={styles.question}>{item.question}</Text>
+          </TouchableOpacity>
+          {!item.isCollapsed ? (
+            <Text style={styles.dropdown}>{item.answer}</Text>
+          ) : null}
+        </View>
+      ))}
     </View>
-  )
+  );
 };
 
 const styles = StyleSheet.create({
-  text: {
+  question: {
     fontSize: 30,
+    textAlign: 'left',
+    margin: 20,
+    borderBottomWidth: 1, // Add a border for a visual hint
+  },
+  dropdown: {
+    fontSize: 20,
+    textAlign: 'left',
+    margin: 20,
   },
 });
 
 export default HelpScreen;
+
