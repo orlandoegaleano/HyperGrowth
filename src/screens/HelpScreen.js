e0ba5acc0052ae40ebce4295607ee56edf038025
emejia3 <emejia3@uno.edu>
2023-11-06 01:26:28 -0600

    Updated HelpScreen.js to have FAQ and General Information

--- src/screens/HelpScreen.js
+++ src/screens/HelpScreen.js
@@ -1,19 +1,21 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import NavBar from '../components/NavBar';
+import GeneralInformation from '../components/GeneralInformation';
+import FAQ from '../components/FAQ';
 
 const HelpScreen = () => {
   const initialData = [
     { question: 'Question 1', answer: 'Answer 1', isCollapsed: true },
     { question: 'Question 2', answer: 'Answer 2', isCollapsed: true },
     // Add more questions, answers, and isCollapsed properties as needed
-    //This is just for testing until we get everything else done.
+    // This is just for testing until we get everything else done.
   ];
 
   const [questions, setQuestions] = useState(initialData);
+  const [activeTab, setActiveTab] = useState('FAQ'); // 'FAQ' or 'GeneralInformation'
 
   const toggleCollapse = (index) => {
-    // Create a copy of the questions array
     const updatedQuestions = [...questions];
     updatedQuestions[index].isCollapsed = !updatedQuestions[index].isCollapsed;
     setQuestions(updatedQuestions);
@@ -22,34 +24,59 @@ const HelpScreen = () => {
   return (
     <View>
       <NavBar />
-      {/* Map over the questions array and render each question */}
-      {questions.map((item, index) => (
-        <View key={index}>
-          <TouchableOpacity onPress={() => toggleCollapse(index)}>
-            <Text style={styles.question}>{item.question}</Text>
-          </TouchableOpacity>
-          {!item.isCollapsed ? (
-            <Text style={styles.dropdown}>{item.answer}</Text>
-          ) : null}
-        </View>
-      ))}
+      <View style={styles.tabButtons}>
+        <TouchableOpacity onPress={() => setActiveTab('FAQ')}>
+          <Text style={activeTab === 'FAQ' ? styles.activeTab : styles.inactiveTab}>
+            FAQ
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setActiveTab('GeneralInformation')}>
+          <Text style={activeTab === 'GeneralInformation' ? styles.activeTab : styles.inactiveTab}>
+            General Information
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      {activeTab === 'FAQ' ? (
+  <FAQ questions={questions} toggleCollapse={toggleCollapse} styles={styles} />
+) : (
+  <GeneralInformation styles={styles} />
+)}
+
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  tabButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+  },
+  activeTab: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'blue',
+  },
+  inactiveTab: {
+    fontSize: 18,
+    color: 'gray',
+  },
   question: {
     fontSize: 30,
     textAlign: 'left',
     margin: 20,
-    borderBottomWidth: 1, // Add a border for a visual hint
+    borderBottomWidth: 1,
   },
   dropdown: {
     fontSize: 20,
     textAlign: 'left',
     margin: 20,
   },
+  generalInfo: {
+    fontSize: 20,
+    margin: 20,
+  },
 });
 
 export default HelpScreen;
-
